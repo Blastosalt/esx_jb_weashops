ce227580e0ec3dc374a58e57dea1b1952b3a1875
Blastosalt <59582132+Blastosalt@users.noreply.github.com>
2020-02-13 02:56:27 +0100

    Add files via upload

---
+++ esx_jb_weashops/locales/it.lua
@@ -0,0 +1,13 @@
+Locales ['it'] = {
+
+  ['buy_license'] = 'Vuoi acquistare il porto d\'armi?',
+  ['yes'] = 'Si',
+  ['no'] = 'Per acquistare la licenza fai richiesta alla polizia',
+  ['buy'] = 'Acquistato ',
+  ['not_enough_black'] = 'Non hai abbastanza soldi sporchi',
+  ['not_enough'] = 'Non hai abbastanza soldi',
+  ['shop'] = 'Armi',
+  ['shop_menu'] = 'Premi ~INPUT_CONTEXT~ per accedere alla lista delle armi.',
+  ['map_blip'] = 'Armeria',
+
+}
